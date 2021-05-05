e8faf3942ba48f8ca2c213b4fa1f4d6fa822e7a5
RosameliaCarioni <72268666+RosameliaCarioni@users.noreply.github.com>
2021-05-05 09:54:31 +0200

    Update State.java

--- MergesSolvers/State.java
+++ MergesSolvers/State.java
@@ -41,84 +41,21 @@ public class State implements StateInterface {
 	 */
 	@Override
 	public StateInterface addMul(double step, RateInterface rate) {
-		// for each planet, calculate the velocity and the position, using the
-		// acceleration and Euler's method
+		// for each planet, calculate the velocity and the position
 		// castedRate is an object containing the accelerations
-		RateOfChange castedRate = (RateOfChange) rate;
+		RateOfChange castedRate = (RateOfChange) rate; //acceleration
+		StateInterface result;
 
-		// new ArrayList to add the celestial body states to after a step
-		ArrayList<CelestialBody> updatedCelestialBodies = new ArrayList<CelestialBody>();
+		//Euler
+		result = EulerSolver.EulerSolve(castedRate,celestialBodies, step );
 
-		/**EULER
-		for (int i = 0; i < celestialBodies.size(); i++) {
-			// v(t2) = v(t1) + timeStep * a(t1)				need to check which acceleration is taking
-			//x(t2) = x(t1) + timeStep * v(t1)
-			// velocity computed using acceleration.
-			Vector3dInterface newVelocity = celestialBodies.get(i).getVelocity().addMul(step, castedRate.accelerations.get(i));
-			// position computed using the newly calculated velocities
+		//Runge Kutta
+		//result = RungeKuttaSolver.RKuttaSolve(castedRate,celestialBodies, step );
 
-			Vector3dInterface newPosition = celestialBodies.get(i).getLocation().addMul(step, celestialBodies.get(i).getVelocity());
-			updatedCelestialBodies.add(new CelestialBody(celestialBodies.get(i).getName(),
-					celestialBodies.get(i).getImage(), celestialBodies.get(i).getMass(), newPosition, newVelocity));
-
-		}
-	*/
-
-		/**Runge Kutta
-		for(int i=0; i < celestialBodies.size(); i++) {
-
-			Vector k1 = new Vector(celestialBodies.get(i).getVelocity().getX(), celestialBodies.get(i).getVelocity().getY(), celestialBodies.get(i).getVelocity().getZ());
-			//Vector k1 = (Vector) celestialBodies.get(i).getVelocity().mul(step);
-			Vector k2 = (Vector) castedRate.accelerations.get(i).mul(step / 2.0).add(k1).add(k1.divide(2.0));
-			Vector k3 = (Vector) castedRate.accelerations.get(i).mul(step / 2.0).add(k1).add(k2.divide(2.0));
-			Vector k4 = (Vector) castedRate.accelerations.get(i).mul(step).add(k1).add(k3);
-			Vector change = (Vector) k1.add(k2.mul(2.0)).add(k3.mul(2.0)).add(k4);
-
-			change = (Vector) change.divide(6.0).mul(step / 2.0);
-
-			Vector3dInterface newPosition = celestialBodies.get(i).getLocation().add(change);
-			Vector3dInterface newVelocity = celestialBodies.get(i).getVelocity().add(castedRate.accelerations.get(i).mul(step ));
-			updatedCelestialBodies.add(new CelestialBody(celestialBodies.get(i).getName(),
-					celestialBodies.get(i).getImage(), celestialBodies.get(i).getMass(), newPosition, newVelocity));
-
-		}
-
-		*/
 
 		//Verlet Solver
-		for(int i = 0; i < celestialBodies.size(); i++) {
-			Vector3dInterface curPosition = celestialBodies.get(i).getLocation();
-			Vector3dInterface curVelocity = celestialBodies.get(i).getVelocity();
-			Vector3dInterface curAcceleration = castedRate.accelerations.get(i);
-
-			Vector3dInterface nextPosition = curPosition.addMul(step, curVelocity).addMul(step * step / 2.0, curAcceleration);
-			Vector3dInterface intermediateVelocity = curVelocity.addMul(step / 2.0, curAcceleration);
-			// omit acceleration forces step as we always calculate it before calling update with force method
-			updatedCelestialBodies.add(new CelestialBody(celestialBodies.get(i).getName(),
-					celestialBodies.get(i).getImage(), celestialBodies.get(i).getMass(), nextPosition, intermediateVelocity));
-		}
-
-		NewtonsFunction calculateAccelerations = new NewtonsFunction();
-		StateInterface newState= (StateInterface) new State(updatedCelestialBodies);
-
-		//new accelerations
-		RateOfChange castedRate2 = (RateOfChange) calculateAccelerations.call(0, newState ); //t is not used in method, so sending 0
-
-
-		// recoil that for now all celestial objects store "intermediate" velocity, so we have to update it once again
-		// in order to get velocity as of end of h time jump
-		for (int i = 0; i < updatedCelestialBodies.size(); i++) {
-			Vector3dInterface curVelocity = updatedCelestialBodies.get(i).getVelocity();
-			Vector3dInterface curAcceleration = castedRate2.accelerations.get(i);
-
-			Vector3dInterface nextVelocity = curVelocity.addMul(step / 2.0, curAcceleration);
-			updatedCelestialBodies.get(i).setVelocity(nextVelocity);
-		}
-
-
-		// the state after the step will be the new state, containing the updated
-		// celestial bodies
-		return (StateInterface) new State(updatedCelestialBodies);
+		//result = VerletSolver.VerletSolve(castedRate,celestialBodies, step );
+		return result;
 	}
 
 	/**
