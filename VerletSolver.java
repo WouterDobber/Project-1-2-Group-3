22d9a5f7f5904c6de6b9d6f72cc0d6034c9ef080
Gevorg-Chakrian <72268436+Gevorg-Chakrian@users.noreply.github.com>
2021-05-18 15:02:25 +0200

    Update VerletSolver.java

--- VerletSolver.java
+++ VerletSolver.java
@@ -4,6 +4,18 @@ public class VerletSolver {
     public static StateInterface VerletSolve(RateOfChange castedRate, ArrayList<CelestialBody> celestialBodies, double step) {
         // new ArrayList to add the celestial body states to after a step
         ArrayList<CelestialBody> updatedCelestialBodies = new ArrayList<CelestialBody>();
+        /*
+            * Processes the shot using the Velocity Verlet Method.
+            * This method updates the position and velocity in five steps:
+            * 1 The current acceleration is calculated using the current position and velocity.
+            * 2 The next position is calculated using the current position, velocity and acceleration.
+            * 3 The intermediate velocity is calculated using the current velocity and acceleration.
+            * 4 The next acceleration is calculated using the next position and intermediate velocity.
+            * 5 The next velocity is calculated using the intermediate velocity and the next acceleration.
+            * Sources used:   http://www.physics.udel.edu/~bnikolic/teaching/phys660/numerical_ode/node5.html
+            *                 https://www2.icp.uni-stuttgart.de/~icp/mediawiki/images/5/54/Skript_sim_methods_I.pdf
+            *                 https://www.algorithm-archive.org/contents/verlet_integration/verlet_integration.html
+        */
         for(int i = 0; i < celestialBodies.size(); i++) {
             Vector3dInterface curPosition = celestialBodies.get(i).getLocation();
             Vector3dInterface curVelocity = celestialBodies.get(i).getVelocity();
