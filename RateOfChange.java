de891ca2091060fd2d593fa771fa1a7b8818e709
RosameliaCarioni <72268666+RosameliaCarioni@users.noreply.github.com>
2021-05-25 14:39:59 +0200

    Update RateOfChange.java

--- RateOfChange.java
+++ RateOfChange.java
@@ -14,6 +14,12 @@ public class RateOfChange implements RateInterface {
 		this.accelerations = accelerations;
 	}
 
+	/**
+	 * Gives the RateOfChange of the celestial bodies at the current state after updating them by a multiplication and addition 
+	 * 
+	 * @param other 
+	 * @param scalar 
+	 */
 	public RateOfChange addMul(double scalar, RateOfChange other) {
 		ArrayList<Vector> addRates = new ArrayList<>();
 		ArrayList<Vector> otherRates = other.getRates();
@@ -23,7 +29,12 @@ public class RateOfChange implements RateInterface {
 		}
 		return new RateOfChange(addRates);
 	}
-
+	
+	/**
+	 * Gives the RateOfChange of the celestial bodies at the current state after updating them by a scalar  
+	 *
+	 * @param scalar 
+	 */
 	public RateOfChange mul(double scalar) {
 		ArrayList<Vector> mulRates = new ArrayList<>();
 		for(int i = 0; i < accelerations.size(); i ++) {
@@ -32,7 +43,10 @@ public class RateOfChange implements RateInterface {
 		return new RateOfChange(mulRates);
 	}
 
-
+	/**
+	* Getter: Returns the acelerations 
+	* @return accelerations 
+	*/ 
 	public ArrayList<Vector> getRates() {
 		return accelerations;
 	}
