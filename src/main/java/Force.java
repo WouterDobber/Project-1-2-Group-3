208b07d68edc4f042bf99400282df9cd420719e6
RosameliaCarioni <72268666+RosameliaCarioni@users.noreply.github.com>
2021-06-10 16:20:28 +0200

    Create Force.java

---
+++ src/main/java/Force.java
@@ -0,0 +1,32 @@
+public class Force {
+    private double magnitude;
+    private Vector3dInterface direction;
+    private double force;
+    private final double AREA = 300; //need to CHANGE it
+    private final double PRESSURE_CONSTANT = 1;  //need to CHANGE it
+    private final double dragCoefficient = 0.47; //for a circle
+
+    public Force(double magnitude,  Vector3dInterface direction){
+        this.magnitude = magnitude;
+        this. direction = direction;
+
+        // F = area * Presure wind * drag coefficient
+        // if circle, drag coefficient = 0.47
+        // pressure wind = constant * v^2
+        double pressureWind = PRESSURE_CONSTANT * magnitude*magnitude;
+        this.force = AREA * pressureWind * dragCoefficient;
+
+    }
+
+    public double getForce(){
+        return force;
+    }
+
+    public double getMagnitude(){
+        return magnitude;
+    }
+
+    public Vector3dInterface getDirection(){
+        return direction;
+    }
+}
