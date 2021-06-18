c474837f0f33b82742a36b43ed8926b84998d868
RosameliaCarioni <72268666+RosameliaCarioni@users.noreply.github.com>
2021-06-18 15:33:53 +0200

    Create PhysicsEngine.java

---
+++ src/main/java/PhysicsEngine.java
@@ -0,0 +1,46 @@
+//PHYSICS ENGINE: note that vectors are treated as: x, y, theta or angle.
+public class PhysicsEngine {
+
+    public static  final double  GRAVITATIONAL_FORCE = -1.352;
+
+    /**
+     *
+     * @param lander
+     * @param bottomThursterForce
+     * @param controllersDirection
+     * @return new vector with updated accelerations in x, y and z (angular acceleration)
+     */
+    public static Vector calculateAccelerations (Lander lander,  double bottomThursterForce, double controllersDirection){
+        double accelerationX = bottomThursterForce * Math.sin(lander.getAngle());
+        double accelerationY = bottomThursterForce * Math.cos(lander.getAngle()) + GRAVITATIONAL_FORCE;
+        double accelerationTheta = ( lander.getRadius() * lander.getMaxControllerForce() * controllersDirection ) / lander.getMomentumOfInertia();
+        return new Vector(accelerationX, accelerationY, accelerationTheta);
+    }
+
+    /**
+     * Updates the velocity and positons of lander (x, y and angle)
+     * @param lander
+     * @param step
+     * @param acceleration
+     * @return  new Lander
+     */
+    public static Lander eulerSolver (Lander lander,  double step, Vector acceleration){
+        Vector newVelocity = (Vector) lander.getVelocity().addMul(step, acceleration);
+        Vector newPosition = (Vector) lander.getPosition().addMul(step, lander.getVelocity());
+        return new Lander(newVelocity, newPosition, lander.getMass(), lander.getRadius(), lander.getMaxControllerForce() );
+    }
+
+    /**
+     * It does one step with Euler solver updating the accelerations
+     * @param lander
+     * @param step
+     * @param bottomThrusterForce
+     * @param controllersDirection
+     * @return new lander
+     */
+    public static Lander step (Lander lander, double step, double bottomThrusterForce, double controllersDirection ){
+        Vector acceleration = calculateAccelerations(lander, bottomThrusterForce, controllersDirection);
+        return  eulerSolver(lander, step, acceleration);
+    }
+    
+}
