662388a4fe7588132825cdf9cec4889351e59c03
RosameliaCarioni <72268666+RosameliaCarioni@users.noreply.github.com>
2021-06-19 01:20:01 +0200

    Update Wind.java

--- src/main/java/Wind.java
+++ src/main/java/Wind.java
@@ -1,26 +1,29 @@
 import java.util.Random;
 
 public class Wind {
+    final static  double AREA = 78; //m2
+    final static double PRESSURE_CONSTANT = 0.00256;  //need to CHANGE it
+    final static double dragCoefficient = 0.47; //for a circle
     /**
-     * Method calculates the force of the wind based on a specific height
+     * Method calculates the force of the wind based on a specific height, Assuming only possible one direction
+     * for it in the x axis either right to left or left to right
      *
      * @param height   of the lander, should be given in meters,
      * @param strength desired strength of the wind, given in %
-     * @return the force of the wind on the lander
+     * @return the force of the wind on the lander, which is a double in Newton
      */
-    public static Force wind(double height, double strength) { //strength is a %
+    public static double wind(double height, double strength) { //strength is a %
         height = height / 1000;  //dividing to transform it to km
         Random rand = new Random();  //based on random, this will define the strength of the wind
 
-     //Adding randomness with gaussian distribution so that the standard deviation can increase or decrease the desired wind by 50%
-        double gauss = (rand.nextGaussian() /2 ) +1;
+        //Adding randomness with gaussian distribution so that the standard deviation can increase or decrease the desired wind by 50%
+        double gauss = (rand.nextGaussian() /5 ) +1;
         if (gauss < 0) {
             gauss = 0;
         }
         strength *= gauss;
-        
+
         double velocityWind = 0; // magnitude
-        Vector direction;
 
         if (height <= 150 && height > 120) {
             velocityWind = (100 * strength) / 100; //based on report, lander started recording from 150 but strongest one was in 120, so randomly choosen 100 to be strongest possible
@@ -30,30 +33,45 @@ public class Wind {
             //call other method
             velocityWind = linearCalculateWind(120, 100, strength, height) / 100;
         } else if (height <= 100 && height >= 60) {
-            velocityWind = 0; //no wind
+            velocityWind = (0.3 * strength) / 100; //no wind
         } else if (height < 60 && height > 0) {
             velocityWind = linearCalculateWind(60, 1, strength, height) / 100;
         } else { //height ==0
             velocityWind = (0.3 * strength) / 100;
         }
 
+        // https://www.wikihow.com/Calculate-Wind-Load
+        // F = area * Presure wind * drag coefficient
+        // if circle, drag coefficient = 0.47
+        // pressure wind = constant * v^2
+        double pressureWind = PRESSURE_CONSTANT * velocityWind*velocityWind; //velocity*1000 to transform it to meters
+        double windForce = AREA * pressureWind * dragCoefficient;
+
+
         //direction of the wind changes twice
         double verticalNoise = 0; //DEFINE THIS
         if (height > 6) { //west to east
-            direction = new Vector(velocityWind, verticalNoise, 0);
+            windForce *= 1;
         } else if (height < 6 && height > 0.7) { //east to west
-            direction = new Vector(-velocityWind, verticalNoise, 0);
+            windForce *= -1;
         } else { //west to east
-            direction = new Vector(velocityWind, verticalNoise, 0);
+            windForce *= 1;
         }
 
-        //multiply both velocity and direction with random component
-        return new Force(velocityWind, direction); //velocity*1000 to transform it to meters
+        return  windForce;
     }
 
     public static double linearCalculateWind(int max, int min, double strength, double height) {
         double difference = max - min;
         double positionInRange = height - min; //my position
         return max * strength * (positionInRange / difference);
+    }
+
+    public static void main(String[] args) {
+        double h = 10000;
+        double strength = 100;
+        for(double i = h; i >= 0; i-=500){
+            System.out.println("height - " + i + " ---  " + wind(h, strength));
+        }
     }
 }
