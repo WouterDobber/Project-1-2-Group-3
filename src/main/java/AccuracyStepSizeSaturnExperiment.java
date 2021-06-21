2ae8f320355e800ffb2cc005cb73af6b52f154f7
RosameliaCarioni <72268666+RosameliaCarioni@users.noreply.github.com>
2021-06-21 18:06:51 +0200

    Update AccuracyStepSizeSaturnExperiment.java

--- src/main/java/AccuracyStepSizeSaturnExperiment.java
+++ src/main/java/AccuracyStepSizeSaturnExperiment.java
@@ -1,3 +1,6 @@
+/**This class was used for experiments in Phase 2, the relative error is computed for different step sizes. 
+*
+**/ 
 public class AccuracyStepSizeSaturnExperiment {
     private static double CONVERSION_UNIT_NASA = 0.001;
 
